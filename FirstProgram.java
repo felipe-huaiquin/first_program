803cd2de3402a77ac7c4bd1b8d13a3d61dbb1054
Felipe <fhuaiquing@gmail.com>
2020-03-07 10:34:09 -0300

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class FirstProgram here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FirstProgram
+{
+    public static void main(String[] args){
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I'm 100 years old");
+        System.out.println("I'm based on Burbank, CA");
+    }
+
+}
